7d4336ca5c19092fada1effa3b369a8ba5732821
DaanVanYperen <daan@mostlyoriginal.net>
2014-12-29 16:31:05 +0100

    Find preferred routes for each team.

--- core/src/net/mostlyoriginal/game/component/Routable.java
+++ core/src/net/mostlyoriginal/game/component/Routable.java
@@ -2,6 +2,8 @@ package net.mostlyoriginal.game.component;
 
 import com.artemis.Component;
 import net.mostlyoriginal.game.Path;
+import org.xguzm.pathfinding.NavigationNode;
+import org.xguzm.pathfinding.grid.GridCell;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,7 +12,7 @@ import java.util.List;
 /**
  * @author Daan van Yperen
  */
-public class Routable extends Component {
+public class Routable extends Component implements NavigationNode {
 	public HashMap<Team, List<Path>> paths = new HashMap<>();
 
 	public Routable() {
@@ -18,4 +20,91 @@ public class Routable extends Component {
 			paths.put(team,new ArrayList<Path>());
 		}
 	}
+
+	/* for path finders*/
+	private float f, g, h;
+	private boolean isWalkable = true;
+	private int closedOnJob, openedOnJob;
+	private NavigationNode parent;
+
+	//for BTree
+	private int index;
+
+	@Override
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+
+	@Override
+	public int getIndex() {
+		return index;
+	}
+
+	public boolean isWalkable() {
+		return isWalkable;
+	}
+
+	public void setWalkable(boolean isWalkable) {
+		this.isWalkable = isWalkable;
+	}
+
+	public float getF() {
+		return f;
+	}
+
+	public void setF(float f) {
+		this.f = f;
+	}
+
+	@Override
+	public float getG() {
+		return g;
+	}
+
+	@Override
+	public void setG(float g) {
+		this.g = g;
+	}
+
+	@Override
+	public float getH() {
+		return h;
+	}
+
+	@Override
+	public void setH(float h) {
+		this.h = h;
+	}
+
+	@Override
+	public NavigationNode getParent() {
+		return parent;
+	}
+
+	@Override
+	public void setParent(NavigationNode parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public int getClosedOnJob() {
+		return closedOnJob;
+	}
+
+	@Override
+	public void setClosedOnJob(int closedOnJob) {
+		this.closedOnJob = closedOnJob;
+	}
+
+	@Override
+	public int getOpenedOnJob() {
+		return openedOnJob;
+	}
+
+	@Override
+	public void setOpenedOnJob(int openedOnJob) {
+		this.openedOnJob = openedOnJob;
+	}
+
 }
--- core/src/net/mostlyoriginal/game/system/PreferredRouteCalculationSystem.java
+++ core/src/net/mostlyoriginal/game/system/PreferredRouteCalculationSystem.java
@@ -7,15 +7,17 @@ import com.artemis.EntitySystem;
 import com.artemis.annotations.Wire;
 import com.artemis.utils.ImmutableBag;
 import net.mostlyoriginal.api.component.basic.Pos;
+import net.mostlyoriginal.game.Path;
 import net.mostlyoriginal.game.component.Routable;
 import net.mostlyoriginal.game.component.Team;
 import net.mostlyoriginal.game.manager.LayerManager;
 import net.mostlyoriginal.game.manager.NavigationGridManager;
-import org.xguzm.pathfinding.PathFinder;
+import org.xguzm.pathfinding.*;
 import org.xguzm.pathfinding.finders.AStarFinder;
-import org.xguzm.pathfinding.grid.GridCell;
-import org.xguzm.pathfinding.grid.NavigationGrid;
-import org.xguzm.pathfinding.grid.finders.GridFinderOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Daan van Yperen
@@ -27,20 +29,84 @@ public class PreferredRouteCalculationSystem extends EntitySystem {
 
 	private boolean resolved = false;
 
+	private HashMap<Team, TeamGraph> teamGraphs = new HashMap<>();
+
 	protected ComponentMapper<Pos> mPos;
 	protected ComponentMapper<Routable> mRoutable;
-	private PathFinder<GridCell> finder;
+	private PathFinder<Routable> finder;
 	private NavigationGridManager navigationGridManager;
+	private Team pathfindTeam;
 
 
 	public PreferredRouteCalculationSystem() {
 		super(Aspect.getAspectForAll(Routable.class, Pos.class));
 	}
 
+	public final class TeamGraph implements NavigationGraph<Routable> {
+
+		private Team team;
+
+		public TeamGraph(Team team) {
+			this.team = team;
+		}
+
+		@Override
+		public List<Routable> getNeighbors(Routable node) {
+			ArrayList<Routable> list = new ArrayList<Routable>();
+
+			// get all neighbours for team.
+			for (Path path : node.paths.get(team)) {
+				Routable routable = getRoutable(path);
+				if (routable != null) {
+					list.add(routable);
+				}
+			}
+
+			return list;
+		}
+
+		@Override
+		public List<Routable> getNeighbors(Routable node, PathFinderOptions opt) {
+			return getNeighbors(node);
+		}
+
+		@Override
+		public float getMovementCost(Routable node1, Routable node2, PathFinderOptions opt) {
+
+			// get all neighbours for team.
+			for (Path path : node1.paths.get(team)) {
+				Routable routable = getRoutable(path);
+				if ( routable == node2 ) {
+					float l = path.getLength() * 0.001f;
+					return l*l*l*l*l*l;
+				}
+			}
+
+			return 999999;
+		}
+
+		@Override
+		public boolean isWalkable(Routable node) {
+			return node.isWalkable();
+		}
+	}
+
+	private Routable getRoutable(Path path) {
+		return path != null && path.destination != null && path.destination.isActive() ? mRoutable.get(path.destination.get()) : null;
+	}
+
 	@Override
 	protected void initialize() {
-		finder = new AStarFinder<>(GridCell.class, new GridFinderOptions());
+		PathFinderOptions pathFinderOptions = new PathFinderOptions() {
+		};
+		pathFinderOptions.heuristic = new Heuristic() {
+			@Override
+			public float calculate(NavigationNode from, NavigationNode to) {
+				return teamGraphs.get(pathfindTeam).getMovementCost((Routable)from, (Routable)to, null);
+			}
+		};
 
+		finder = new AStarFinder<>(Routable.class,pathFinderOptions);
 		//new NavigationGrid();
 	}
 
@@ -49,6 +115,10 @@ public class PreferredRouteCalculationSystem extends EntitySystem {
 
 		if (!resolved) {
 			resolved = true;
+
+			for (Team team : Team.values()) {
+				teamGraphs.put(team, new TeamGraph(team) );
+			}
 
 			// eliminate duplicate routes using the following algorithm.
 			// long paths get a small penalty.
@@ -64,10 +134,36 @@ public class PreferredRouteCalculationSystem extends EntitySystem {
 					final Entity entityA = entities.get(a);
 					final Entity entityB = entities.get(b);
 
-					Routable routable = mRoutable.get(entityA);
-					routable.paths.get(Team.ALIEN).get(0).preferred = true;
-					routable.paths.get(Team.MARINE).get(0).preferred = true;
+					Routable routableA = mRoutable.get(entityA);
+					Routable routableB = mRoutable.get(entityB);
+
+					for (Team team : Team.values()) {
+
+						// bit of a hack to get the right distances for each team.
+						pathfindTeam = team;
+
+						List<Routable> path = finder.findPath(routableA, routableB, teamGraphs.get(team));
+						if ( path != null ) {
+							path.add(0, routableA);
+
+							for (int i = 1; i < path.size(); i++) {
+								markPreferred( team, path.get(i-1), path.get(i));
+							}
+						}
+					}
+
 				}
+			}
+		}
+	}
+
+	/** Mark team route from src to dst as the preferred route. */
+	private void markPreferred(Team team, Routable src, Routable dst) {
+
+		for (Path path : src.paths.get(team)) {
+			Routable routable = getRoutable(path);
+			if (routable == dst ) {
+				path.preferred = true;
 			}
 		}
 	}
