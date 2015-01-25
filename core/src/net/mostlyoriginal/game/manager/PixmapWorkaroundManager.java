9d40c6adb9cfebcf93b83be6665d6ce7bb11030b
DaanVanYperen <daan@mostlyoriginal.net>
2015-01-25 21:46:15 +0100

    Missed some commits.

---
+++ core/src/net/mostlyoriginal/game/manager/PixmapWorkaroundManager.java
@@ -0,0 +1,9 @@
+package net.mostlyoriginal.game.manager;
+
+/**
+ * Work around the pixmap bug on GWT where getPixel caches data.
+ *
+ * @author Daan van Yperen
+ */
+public class PixmapWorkaroundManager {
+}
