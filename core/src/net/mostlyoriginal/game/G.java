d046d7255c71c97a9a334b678516c35e3fd9614e
DaanVanYperen <daan@mostlyoriginal.net>
2015-01-31 16:20:03 +0100

    Clean up constants.

--- core/src/net/mostlyoriginal/game/G.java
+++ core/src/net/mostlyoriginal/game/G.java
@@ -5,21 +5,14 @@ package net.mostlyoriginal.game;
  */
 public class G {
 
-    public static final int CANVAS_WIDTH = 800;
+	public static String TITLE = "NS2 SCC Profiler";
+
+	public static final int CANVAS_WIDTH = 800;
     public static final int CANVAS_HEIGHT = 800;
 
-    // speed (in units per second).
-    public static final float marineSpeedSlow = 6f;
-    public static final float MARINE_SPEED_FAST = 6.25f;
+	// speed (in units per second)
     public static final float MARINE_SPEED_AVG = 6.25f; // marine can keep this speed consistently.
-    public static final float alienSpeedSlow  = 7.5f;
-    public static final float ALIEN_SPEED_FAST = 11f;
-    public static final float ALIEN_SPEED_AVG = 9f; // speed is harder for aliens.
+	public static final float ALIEN_SPEED_AVG = 9f; // speed is harder for aliens.
 
-    public static final float UNITS_PER_PIXEL = 0.62f; // CAGED
-    //public static final float UNITS_PER_PIXEL = (1/0.95f) * (1/1.75f); //
-
-    public static int MARINE_TEAM_SIZE = 8;
-    public static int ALIEN_TEAM_SIZE = 8;
-    public static String TITLE = "NS2 Yardstick";
+    public static final float UNITS_PER_PIXEL = 0.62f; // based on caged. Will vary per map.
 }
